2551551eb6aceeea14e17a9c46758dda152bb5cb
lukem <mluke25@aischennai.org>
2025-06-26 17:39:40 -0400

    fix: updated database_setup to include metadata

--- ingestion/chromainit/database_setup.py
+++ ingestion/chromainit/database_setup.py
@@ -4,10 +4,14 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.indexes import SQLRecordManager, index
 import os
 import dotenv
+from chromadb.api import Settings
+import chromadb
+
+from ingestion.chromainit.chroma_collection_metadata import CollectionMetadata, get_current_time_in_iso_8601_format_utc
 
 
 def get_embeddings(model_name: str = "Qwen/Qwen3-Embedding-0.6B") -> Embeddings:
-    # Running on CPU since I am using a macbook. 
+    # Running on CPU since I am using a macbook. You should change it depending on the hardware.
     model_kwargs = {'device': 'cpu'}
     return HuggingFaceEmbeddings(
         model_name=model_name,
@@ -16,23 +20,45 @@ def get_embeddings(model_name: str = "Qwen/Qwen3-Embedding-0.6B") -> Embeddings:
 
 
 def setup_vector_store(collection_name: str, embeddings: Embeddings, persist_directory: str) -> Chroma:
-    return Chroma(
-        collection_name=collection_name,
-        embedding_function=embeddings,
-        persist_directory=persist_directory
+    client = chromadb.PersistentClient(path=persist_directory, settings=Settings(anonymized_telemetry=False))
+
+    collection_metadata = CollectionMetadata(
+        data_source="SEC Filings",
+        embedding_model_name="Qwen/Qwen3-Embedding-0.6B",
+        created_by="Luke M",
+        project_id="FinQuery",
+        parser_version="1.1",
+        created_at_iso=get_current_time_in_iso_8601_format_utc()
     )
 
+    collection = client.get_or_create_collection(
+        name=collection_name,
+        embedding_function=None,
+        metadata=collection_metadata.model_dump(),
+    )
 
-def setup_record_manager(collection_name: str):
+    print(f"Successfully retrieved/created collection '{collection.name}' with metadata:")
+    print(collection.metadata)
+
+    vector_store = Chroma(
+        client=client,
+        collection_name=collection_name,
+        embedding_function=embeddings,
+    )
+
+    return vector_store
+
+
+def setup_record_manager(collection_name: str) -> SQLRecordManager:
     dotenv.load_dotenv()
 
     namespace = f"chroma/{collection_name}"
 
-    database_url = f"postgresql+psycopg2://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
+    db_url = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
     record_manager = SQLRecordManager(
         namespace,
-        db_url=database_url
+        db_url=db_url
     )
 
     record_manager.create_schema()
